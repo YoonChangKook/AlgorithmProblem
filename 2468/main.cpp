d5218bed10878a55efc306462566a3972688bf7a
YoonchangKook <yoonchang.kook@nts-corp.com>
2019-05-07 10:46:40 +0900

    백준 2468번 union-find 자료구조 및 최적화

--- 2468/main.cpp
+++ 2468/main.cpp
@@ -1,90 +1,111 @@
 #include <stdio.h>
-#include <queue>
+#include <algorithm>
+#include <functional>
 #include <vector>
 
 #define MAX 100
 
 using namespace std;
 
-typedef pair<int, int> Pair;
-Pair operator+(const Pair& p1, const Pair& p2)
+// 좌표
+typedef pair<int, int> Pos;
+Pos operator+(const Pos& p1, const Pos& p2)
 {
-	return { p1.first + p2.first, p1.second + p2.second };
+	return{ p1.first + p2.first, p1.second + p2.second };
 }
+// 높이, 좌표 쌍
+typedef pair<int, Pos> Height;
 
 int n;
-int map[MAX][MAX];
+vector<Height> heights;
 int max_value = 0;
-Pair dir[4] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
+Pos dir[4] = { {0, 1}, {1, 0}, {0, -1}, {-1, 0} };
 
-int CalcAreaNum(int height);
+int uf_find(int group[], int u)
+{
+	if (u == group[u]) return u;
+	return group[u] = uf_find(group, group[u]);
+}
+
+bool uf_union(int group[], int u, int v)
+{
+	u = uf_find(group, u);
+	v = uf_find(group, v);
+
+	if (u == v)
+		return false;
+
+	group[u] = v;
+
+	return true;
+}
+
+int getIdx(const Pos& pos)
+{
+	return pos.first * n + pos.second;
+}
+
+bool isValidPos(const Pos& pos)
+{
+	return 0 <= pos.first && pos.first < n && 0 <= pos.second && pos.second < n;
+}
+
+int calcAreaNum()
+{
+	int group[MAX * MAX]; // disjoint set으로 나눈 그룹
+	bool available[MAX * MAX] = { false, }; // 해당 좌표가 기준 높이 이상인지 체크
+	int group_count = 0, max_group_count = 0;
+
+	// 초기화
+	for (int i = 0; i < n * n; i++)
+		group[i] = i;
+
+	// 그룹 계산
+	for (int i = 0, next; i < heights.size() - 1; i = next)
+	{
+		// 다음 높이 인덱스 찾기
+		for (next = i; next < heights.size() && heights[i].first == heights[next].first; next++);
+		for (int j = i; j < next; j++)
+		{
+			available[getIdx(heights[j].second)] = true;
+			group_count++;
+			for (int k = 0; k < 4; k++)
+			{
+				Pos next_pos = heights[j].second + dir[k];
+				if (isValidPos(next_pos) && available[getIdx(next_pos)])
+					group_count -= uf_union(group, getIdx(heights[j].second), getIdx(next_pos));
+			}
+		}
+
+		if (group_count > max_group_count)
+			max_group_count = group_count;
+	}
+
+	return max_group_count;
+}
 
 int main()
 {
 	// 입력
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
+	Pos pos; int h;
+	for (pos.first = 0; pos.first < n; pos.first++)
 	{
-		for (int j = 0; j < n; j++)
+		for (pos.second = 0; pos.second < n; pos.second++)
 		{
-			scanf("%d", &map[i][j]);
-			if (map[i][j] > max_value)
-				max_value = map[i][j];
+			scanf("%d", &h);
+			heights.push_back({ h, pos });
 		}
 	}
 
+	// 높이 순으로 좌표 정렬
+	sort(heights.begin(), heights.end(), greater<Height>());
+
 	// 계산
-	int max_area_num = 0;
-	for (int i = 0; i < max_value; i++)
-	{
-		int area_num = CalcAreaNum(i);
-		if (max_area_num <= area_num)
-			max_area_num = area_num;
-	}
+	int max_area_num = calcAreaNum();
 
 	// 출력
 	printf("%d\n", max_area_num);
 
 	return 0;
-}
-
-int CalcAreaNum(int height)
-{
-	int area_num = 0;
-	int visited[MAX][MAX] = { 0, };
-	
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			// 이미 검사했거나 높이가 낮으면 통과
-			if (visited[i][j] != 0 || map[i][j] <= height)
-				continue;
-
-			queue<Pair> calc_queue;
-			visited[i][j] = 1;
-			calc_queue.push({ i, j });
-			while (!calc_queue.empty())
-			{
-				Pair pos = calc_queue.front();
-				calc_queue.pop();
-				for (int k = 0; k < 4; k++)
-				{
-					Pair next_pos = pos + dir[k];
-					if (0 <= next_pos.first && next_pos.first < n &&
-						0 <= next_pos.second && next_pos.second < n &&
-						visited[next_pos.first][next_pos.second] == 0 &&
-						map[next_pos.first][next_pos.second] > height)
-					{
-						calc_queue.push(next_pos);
-						visited[next_pos.first][next_pos.second] = 1;
-					}
-				}
-			}
-
-			area_num++;
-		}
-	}
-
-	return area_num;
 }
