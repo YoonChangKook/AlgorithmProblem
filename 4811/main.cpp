23d17459dea083501b6a7a4bbc74a0dadc16b1d6
YoonChangKook <kkyc8909@naver.com>
2019-05-12 04:58:56 +0900

    백준 4811 카탈린 수 이용
     - 카탈린 수 점화식 중 하나인 홀로노믹 수열 이용

--- 4811/main.cpp
+++ 4811/main.cpp
@@ -2,30 +2,16 @@
 
 #define MAX_N 30
 
+int n;
+long long dp[MAX_N] = { 1LL, };
+
 int main()
 {
-	while (true)
-	{
-		int n;
-		long long num_counts[MAX_N] = { 0, };
-		long long result = 0;
-		num_counts[0] = 1L; num_counts[1] = 1L;
-
-		// 입력
-		scanf("%d", &n);
-		if (n == 0)
-			break;
-
-		// 계산
-		for (int i = 2; i < n - 1; i++) // 각 숫자 별 개수
-			for (int j = 0; j < i; j++)
-				num_counts[j + 1] += num_counts[j];
-		for (int i = 0; i < n - 1; i++) // 숫자 개수로 총합 집계
-			result += num_counts[i] * (n - i);
-
-		// 출력
-		printf("%lld\n", result == 0 ? 1 : result);
-	}
-
-	return 0;
+	// 계산 (카탈린 수, 홀로노믹 수열)
+	for (int i = 1; i < 30; i++)
+		dp[i] = dp[i - 1] * (4 * i + 2) / (i + 2);
+	// 출력
+	for (; scanf("%d", &n); printf("%lld\n", dp[n - 1]))
+		if (!n)
+			return 0;
 }
