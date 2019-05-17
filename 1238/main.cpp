075e4e72b43712103c5aa0fa747eca6c3ccdb1ee
YoonchangKook <yoonchang.kook@nts-corp.com>
2019-05-17 18:49:01 +0900

    백준 1238번 우선순위 큐, 벡터 추가

---
+++ 1238/main.cpp
@@ -0,0 +1,156 @@
+#include <stdio.h>
+
+template <class T>
+class vector
+{
+private:
+	int capacity;
+	int count;
+	T* arr;
+
+public:
+	vector()
+		: capacity(100), count(0)
+	{
+		arr = new T[capacity];
+	}
+
+	~vector()
+	{
+		delete[] arr;
+	}
+
+	void push_back(const T& obj)
+	{
+		if (capacity <= count - 1)
+			reallocate(capacity * 2);
+		arr[count++] = obj;
+	}
+
+	void insert(const T& obj, int index)
+	{
+		if (capacity <= count - 1)
+			reallocate(capacity * 2);
+
+		for (int i = count; i > index; i--)
+			arr[i] = arr[i - 1];
+		arr[index] = obj;
+	}
+
+	void remove(int index)
+	{
+		for (int i = index; i < count - 1; i++)
+			arr[i] = arr[i + 1];
+	}
+
+	T pop_back()
+	{
+		return arr[--count];
+	}
+
+	T& operator[] (int index)
+	{
+		return arr[index];
+	}
+
+	int get_count()
+	{
+		return count;
+	}
+
+private:
+	void reallocate(int new_capacity)
+	{
+		capacity = new_capacity;
+
+		T* temp_arr = arr;
+		arr = new T[new_capacity];
+		for (int i = 0; i < count; i++)
+			arr[i] = temp_arr[i];
+		delete[] temp_arr;
+	}
+};
+
+template <class T>
+class priority_queue
+{
+private:
+	vector<T> nodes;
+
+public:
+	void push(const T& node)
+	{
+		nodes.push_back(node);
+		quicksort(nodes, 0, nodes.get_count() - 1);
+	}
+
+	T top()
+	{
+		return nodes[nodes.get_count() - 1];
+	}
+
+	T pop()
+	{
+		return nodes.pop_back();
+	}
+
+private:
+	void quicksort(vector<T>& arr, int start, int end)
+	{
+		int pivot = start;
+		int left = start, right = end;
+
+		while (left < right)
+		{
+			// top, pop 함수가 맨 뒤의 인덱스부터 뽑기때문에 역순 정렬
+			while (left < right && arr[right] < arr[pivot]) right--;
+			while (left < right && arr[pivot] < arr[left]) left++;
+			swap(arr[left], arr[right]);
+		}
+		swap(arr[pivot], arr[left]);
+
+		if (start < left)
+			quicksort(arr, start, left - 1);
+		if (right < end)
+			quicksort(arr, left + 1, end);
+	}
+
+	void swap(T& t1, T& t2)
+	{
+		T temp = t1;
+		t1 = t2;
+		t2 = temp;
+	}
+};
+
+// 정점까지 가는 가중치를 담은 구조체
+typedef struct Vertex
+{
+	int node;
+	int weight;
+
+	bool operator < (const Vertex& other)
+	{
+		return weight < other.weight;
+	}
+} Vertex;
+
+vector<vector<Vertex>> graph;
+priority_queue<Vertex> dists;
+
+int main()
+{
+	// 우선순위 큐, 벡터 테스트
+	priority_queue<int> test;
+	int arr[] = { 5, 3, 2, 1, 4 };
+	int arr_size = sizeof(arr) / sizeof(int);
+
+	for (int i = 0; i < arr_size; i++)
+		test.push(arr[i]);
+
+	for (int i = 0; i < arr_size; i++)
+		printf("%d ", test.pop());
+	printf("\n");
+
+	return 0;
+}
