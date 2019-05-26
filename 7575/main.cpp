4b7a61900fa4d7cff3e4df09a034de118f56f1d4
YoonChangKook <kkyc8909@naver.com>
2019-05-26 18:18:25 +0900

    백준 7575번
     - kmp 알고리즘을 이용함
     - 오름차순 내림차순 pi 배열을 따로 두어 검색함

--- 7575/main.cpp
+++ 7575/main.cpp
@@ -23,6 +23,14 @@ public:
 		delete[] arr;
 	}
 
+	vector(const vector<T>& other)
+		: capacity(other.capacity), count(other.count)
+	{
+		arr = new T[capacity];
+		for (int i = 0; i < capacity; i++)
+			arr[i] = other.arr[i];
+	}
+
 	void push_back(const T& item)
 	{
 		if (capacity <= count)
@@ -75,9 +83,9 @@ private:
 
 public:
 	queue(int capacity)
-		: capacity(capacity), start(0), end(0)
+		: capacity(capacity + 1), start(0), end(0)
 	{
-		arr = new T[capacity];
+		arr = new T[this->capacity];
 	}
 
 	~queue()
@@ -87,19 +95,24 @@ public:
 
 	void push(const T& item)
 	{
-		arr[end] = item;
 		end = (end + 1) % capacity;
+		arr[end] = item;
 	}
 
 	T pop()
 	{
-		return arr[start];
 		start = (start + 1) % capacity;
+		return arr[start];
 	}
 
 	const T& front() const
 	{
-		return arr[start];
+		return arr[(start + 1) % capacity];
+	}
+
+	const T& operator[](int index) const
+	{
+		return arr[(start + 1 + index) % capacity];
 	}
 
 	int size() const
@@ -109,12 +122,112 @@ public:
 		else
 			return capacity + (end - start);
 	}
+
+	bool empty() const
+	{
+		return end == start;
+	}
+
+	bool full() const
+	{
+		return (end + 1) % capacity == start;
+	}
 };
 
 int n, k;
 vector<int> program_codes[MAX_PROGRAM_COUNT];
-vector<int> sum_ascd[MAX_PROGRAM_COUNT];
-vector<int> sum_desd[MAX_PROGRAM_COUNT];
+int pi_ascd[MAX_PROGRAM_LENGTH];
+int pi_dscd[MAX_PROGRAM_LENGTH];
+
+// 주어진 패턴에 대한 pi계산
+void calc_pi(const queue<int>& pattern)
+{
+	int ascd = 0, dscd = 0;
+	for (int i = 1; i < pattern.size(); i++)
+	{
+		// 오름차순
+		while (ascd > 0 && pattern[i] != pattern[ascd])
+			ascd = pi_ascd[ascd - 1];
+
+		if (pattern[i] == pattern[ascd])
+			pi_ascd[i] = ++ascd;
+
+		// 내림차순
+		while (dscd > 0 && pattern[pattern.size() - 1 - i] != pattern[pattern.size() - 1 - dscd])
+			dscd = pi_dscd[dscd - 1];
+
+		if (pattern[pattern.size() - 1 - i] == pattern[pattern.size() - 1 - dscd])
+			pi_dscd[i] = ++dscd;
+	}
+}
+
+// 코드 배열안에 패턴이 존재하는지 kmp로 검사
+bool calc_kmp(const vector<int>& target_codes, const queue<int>& pattern)
+{
+	int ascd = 0, dscd = 0;
+	for (int i = 0; i < target_codes.size(); i++)
+	{
+		// 오름차순
+		while (ascd > 0 && target_codes[i] != pattern[ascd])
+			ascd = pi_ascd[ascd - 1];
+
+		if (target_codes[i] == pattern[ascd])
+		{
+			if (ascd == pattern.size() - 1)
+				return true;
+			else
+				ascd++;
+		}
+
+		// 내림차순
+		while (dscd > 0 && target_codes[i] != pattern[pattern.size() - 1 - dscd])
+			dscd = pi_ascd[dscd - 1];
+
+		if (target_codes[i] == pattern[pattern.size() - 1 - dscd])
+		{
+			if (dscd == pattern.size() - 1)
+				return true;
+			else
+				dscd++;
+		}
+	}
+
+	return false;
+}
+
+// 모든 코드 배열에 대해 kmp 계산
+bool calc_virus()
+{
+	queue<int> pattern(k);
+	for (int i = 0; i < k - 1; i++)
+		pattern.push(program_codes[0][i]);
+
+	for (int i = k - 1; i < program_codes[0].size(); i++)
+	{
+		int result = true;
+
+		if (pattern.full())
+			pattern.pop();
+		pattern.push(program_codes[0][i]);
+
+		// 패턴 추출
+		calc_pi(pattern);
+
+		// 모든 다른 문자열에 대해 kmp 추출
+		for (int j = 1; j < n; j++)
+		{
+			result = calc_kmp(program_codes[j], pattern);
+			if (!result)
+				break;
+		}
+
+		// 모든 다른 문자열에 대해 일치하는 문자열을 찾으면 리턴
+		if (result)
+			return true;
+	}
+
+	return false;
+}
 
 int main()
 {
@@ -127,11 +240,14 @@ int main()
 		for (int j = 0; j < len; j++)
 		{
 			scanf("%d", &code);
+
+			// 코드 입력
 			program_codes[i].push_back(code);
 		}
 	}
 
-	// 계산
+	// 계산 및 출력
+	printf("%s\n", calc_virus() ? "YES" : "NO");
 
 	return 0;
 }
